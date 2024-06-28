a0929bfb7fe0fbdbe27bd035e31f04679d0b719c
Len Danieo Thomas Mendonca <lenmendonca2003@gmail.com>
2024-06-28 23:01:06 +0530

    Update find-all-anagrams-in-a-string.cpp

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -2,28 +2,28 @@ class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
        vector<int> ans;
-        unordered_map<char,int>sub;
-        for(int i=0;i<p.length();i++){
+        unordered_map<char,int>sub; //stores unique char of substring p in map
+        for(int i=0;i<p.length();i++){ //assigns the occurences of each char in map
             if(sub.count(p[i])){
                 sub[p[i]]++;
             }else{
                 sub[p[i]]=1;
             }
         }
-        int count = sub.size();
+        int count = sub.size(); //when count is 0, it is an anagram
         int i=0,j=0;
-        while(j<s.size()){
+        while(j<s.size()){  //finding out the number of anagrams
             if(sub.count(s[j])){
                 sub[s[j]]--;
                 if(sub[s[j]]==0){
                     count--;
                 }
             }
-            if(j-i+1<p.size()){
+            if(j-i+1<p.size()){ 
                 j++;
             }else if(j-i+1 == p.size()){
                 if(count==0){
-                    ans.push_back(i);
+                    ans.push_back(i); //pushing the index into the result
                 }
                 if(sub.count(s[i])){
                     sub[s[i]]++;
